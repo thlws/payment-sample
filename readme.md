2e439ffd0814ede7126b07dd4165d6322289ade8
Hanley <hanley@thlws.com>
2019-01-14 19:01:26 +0800

    支付宝示例完善

--- readme.md
+++ readme.md
@@ -323,9 +323,34 @@ public class WechatPayController {
 ```java
 package org.thlws.payment.simple.controller;
 
+import cn.hutool.log.Log;
+import cn.hutool.log.LogFactory;
+import com.alipay.api.AlipayConstants;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.thlws.payment.AlipayClient;
+import org.thlws.payment.alipay.core.AlipayCore;
+import org.thlws.payment.alipay.entity.request.AlipayMobileSiteRequest;
+import org.thlws.payment.alipay.entity.request.AlipayRefundRequest;
+import org.thlws.payment.alipay.entity.request.AlipayTradeRequest;
+import org.thlws.payment.alipay.entity.request.AlipayWebSiteRequest;
+import org.thlws.payment.alipay.entity.response.AlipayRefundResponse;
+import org.thlws.payment.alipay.entity.response.AlipayTradeResponse;
+import org.thlws.payment.simple.config.AlipayConfig;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
+
 /**
+ * 支付宝支付示例.
+ * FIXME 示例中 [手机+电脑] 网站支付暂不可用,我这里没有进行相关配置而已。
  * @author HanleyTang
- * @date 2018-12-02
+ * @date 2018 -12-02
  */
 @Controller
 @RequestMapping("/alipay")
@@ -336,14 +361,27 @@ public class AlipayController {
     @Autowired
     private AlipayConfig alipayConfig;
 
+    /**
+     * 支付宝付款测试页.
+     *
+     * @return the string
+     */
     @RequestMapping("/index")
     public String index(){
         return "alipay";
     }
 
-    @RequestMapping("/pay/f2f")
+    /**
+     * 当面付.
+     * 用于线下POS，线上一般不会这么用
+     *
+     * @param amt     the amt
+     * @param barcode the barcode
+     * @return the object
+     */
+    @RequestMapping(value = "/pay/f2f",produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     @ResponseBody
-    public AlipayTradeResponse payFaceToFace(@RequestParam(name = "amt")String amt,
+    public Object payFaceToFace(@RequestParam(name = "amt")String amt,
                                 @RequestParam(name = "barcode")String barcode){
         try {
             AlipayCore.ClientBuilder clientBuilder = new AlipayCore.ClientBuilder();
@@ -359,19 +397,25 @@ public class AlipayController {
             request.setAuthCode(barcode);
             request.setOutTradeNo(System.currentTimeMillis()+"");
             request.setSubject("测试当面付");
-
             AlipayTradeResponse response = AlipayClient.pay(request,alipayCore);
             return response;
-
         } catch (Exception e) {
-            log.error(e);
+            log.error("当面付失败",e);
+            return e;
         }
 
-        return null;
     }
 
+    /**
+     * 手机网站支付. FIXME 暂不可用
+     * 在手机浏览器打开(不能在微信中)完成付款动作.
+     *
+     * @param amt      the amt
+     * @param modelMap the model map
+     * @return the string
+     */
     @RequestMapping("/pay/mobile")
-    public String payInMobileSite(@RequestParam(name = "amt")String amt,
+    public String payInMobileSite(@RequestParam(name = "amt",defaultValue = "0.01")String amt,
                                   ModelMap modelMap){
 
         try {
@@ -382,47 +426,45 @@ public class AlipayController {
                     .setSignType(AlipayConstants.SIGN_TYPE_RSA2).build();
 
             AlipayMobileSiteRequest request = new AlipayMobileSiteRequest();
-            request.setNotifyUrl("http://116.228.212.37:8080/alipay/notify_mobile");
-            request.setReturnUrl("http://localhost:8080/alipay/alipay_sync_mobile");
+            request.setNotifyUrl("http://你的异步处理地址/alipay/notify_mobile");
+            request.setReturnUrl("http://你的同步处理地址/alipay/alipay_sync_mobile");
             AlipayMobileSiteRequest.BizContent bizContent = new AlipayMobileSiteRequest.BizContent();
-            bizContent.setTotalAmount("0.01");
+            bizContent.setTotalAmount(amt);
             bizContent.setSubject("测试H5(手机网页)支付");
             //bizContent.setSeller_id(partner_id);
             bizContent.setProductCode("p0001");
             bizContent.setOutTradeNo(System.currentTimeMillis()+"");
             request.setBizContent(bizContent);
             String html = AlipayClient.payInMobileSite(request,alipayCore);
-
             modelMap.addAttribute("form", html);
-
-
-
         } catch (Exception e) {
-            log.error(e);
+            log.error("手机网站支付失败",e);
         }
-
         return "alipay/alipay_mobile";
     }
 
+    /**
+     * 电脑网站支付.FIXME 暂不可用
+     *
+     * @param amt      the amt
+     * @param modelMap the model map
+     * @return the string
+     */
     @RequestMapping("/pay/web")
-    public String payInWebSite(@RequestParam(name = "amt")String amt,
+    public String payInWebSite(@RequestParam(name = "amt",defaultValue = "0.01")String amt,
                                ModelMap modelMap){
-
-
         try {
-
             AlipayCore.ClientBuilder clientBuilder = new AlipayCore.ClientBuilder();
             AlipayCore alipayCore = clientBuilder.setAlipayPublicKey(alipayConfig.getAlipay_public_key())
                     .setAppId(alipayConfig.getAppid())
                     .setPrivateKey(alipayConfig.getPrivate_key())
                     .setSignType(AlipayConstants.SIGN_TYPE_RSA2).build();
 
-
             AlipayWebSiteRequest request = new AlipayWebSiteRequest();
-            request.setNotifyUrl("http://116.228.212.37:8080/alipay/notify_mobile");
-            request.setReturnUrl("http://localhost:8080/alipay/alipay_sync_web");
+            request.setNotifyUrl("http://你的异步处理地址/alipay/notify_mobile");
+            request.setReturnUrl("http://你的同步处理地址/alipay/alipay_sync_web");
             AlipayWebSiteRequest.BizContent bizContent = new AlipayWebSiteRequest.BizContent();
-            bizContent.setTotalAmount("0.01");
+            bizContent.setTotalAmount(amt);
             bizContent.setSubject("测试电脑网站支付");
             bizContent.setBody("测试");
             bizContent.setProductCode("p0001");
@@ -431,20 +473,25 @@ public class AlipayController {
             modelMap.addAttribute("form", html);
 
         } catch (Exception e) {
-            log.error(e);
+            log.error("电脑网站支付失败",e);
         }
-
 
         return "alipay/alipay_web";
     }
 
-    @RequestMapping("/refund")
+    /**
+     * 退款,适用所有付款方式.
+     *
+     * @param amt        the amt
+     * @param outTradeNo the out trade no
+     * @return the object
+     */
+    @RequestMapping(value = "/refund",produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     @ResponseBody
-    public AlipayRefundResponse refund(@RequestParam(name = "amt")String amt,
+    public Object refund(@RequestParam(name = "amt")String amt,
                          @RequestParam(name = "out_trade_no")String outTradeNo){
 
         try {
-
             AlipayCore.ClientBuilder clientBuilder = new AlipayCore.ClientBuilder();
             AlipayCore alipayCore = clientBuilder.setAlipayPublicKey(alipayConfig.getAlipay_public_key())
                     .setAppId(alipayConfig.getAppid())
@@ -457,15 +504,19 @@ public class AlipayController {
             request.setRefundReason("测试退款");
             AlipayRefundResponse response = AlipayClient.refund(request,alipayCore);
             return response;
-
         } catch (Exception e) {
-            log.error(e);
+            log.error("支付宝退款失败.",e);
+            return e;
         }
-
-        return null;
     }
 
 
+    /**
+     * 手机网站支付同步跳转页面
+     *
+     * @param request the request
+     * @return the string
+     */
     @RequestMapping("/alipay_sync_mobile")
     public String sync_mobile(HttpServletRequest request){
 
@@ -480,9 +531,11 @@ public class AlipayController {
     }
 
 
+    /**
+     * 电脑网站支付同步跳转页面.
+     */
     @RequestMapping("/alipay_sync_web")
     public String sync_web(HttpServletRequest request){
-
         System.out.println("==============sync_web=====================");
         Enumeration<String> its = request.getParameterNames();
         while (its.hasMoreElements()){
@@ -494,7 +547,6 @@ public class AlipayController {
     }
 
 }
-
 ```
 
 ## 翼支付示例
--- src/main/java/org/thlws/payment/simple/controller/AlipayController.java
+++ src/main/java/org/thlws/payment/simple/controller/AlipayController.java
@@ -4,6 +4,7 @@ import cn.hutool.log.Log;
 import cn.hutool.log.LogFactory;
 import com.alipay.api.AlipayConstants;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import org.thlws.payment.alipay.entity.request.AlipayMobileSiteRequest;
 import org.thlws.payment.alipay.entity.request.AlipayRefundRequest;
 import org.thlws.payment.alipay.entity.request.AlipayTradeRequest;
 import org.thlws.payment.alipay.entity.request.AlipayWebSiteRequest;
+import org.thlws.payment.alipay.entity.response.AlipayQueryResponse;
 import org.thlws.payment.alipay.entity.response.AlipayRefundResponse;
 import org.thlws.payment.alipay.entity.response.AlipayTradeResponse;
 import org.thlws.payment.simple.config.AlipayConfig;
@@ -23,8 +25,10 @@ import javax.servlet.http.HttpServletRequest;
 import java.util.Enumeration;
 
 /**
+ * 支付宝支付示例.
+ * FIXME 示例中 [手机+电脑] 网站支付暂不可用,我这里没有进行相关配置而已。
  * @author HanleyTang
- * @date 2018-12-02
+ * @date 2018 -12-02
  */
 @Controller
 @RequestMapping("/alipay")
@@ -35,14 +39,27 @@ public class AlipayController {
     @Autowired
     private AlipayConfig alipayConfig;
 
+    /**
+     * 支付宝付款测试页.
+     *
+     * @return the string
+     */
     @RequestMapping("/index")
     public String index(){
         return "alipay";
     }
 
-    @RequestMapping("/pay/f2f")
+    /**
+     * 当面付.
+     * 用于线下POS，线上一般不会这么用
+     *
+     * @param amt     the amt
+     * @param barcode the barcode
+     * @return the object
+     */
+    @RequestMapping(value = "/pay/f2f",produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     @ResponseBody
-    public AlipayTradeResponse payFaceToFace(@RequestParam(name = "amt")String amt,
+    public Object payFaceToFace(@RequestParam(name = "amt")String amt,
                                 @RequestParam(name = "barcode")String barcode){
         try {
             AlipayCore.ClientBuilder clientBuilder = new AlipayCore.ClientBuilder();
@@ -58,19 +75,25 @@ public class AlipayController {
             request.setAuthCode(barcode);
             request.setOutTradeNo(System.currentTimeMillis()+"");
             request.setSubject("测试当面付");
-
             AlipayTradeResponse response = AlipayClient.pay(request,alipayCore);
             return response;
-
         } catch (Exception e) {
-            log.error(e);
+            log.error("当面付失败",e);
+            return e;
         }
 
-        return null;
     }
 
+    /**
+     * 手机网站支付. FIXME 暂不可用
+     * 在手机浏览器打开(不能在微信中)完成付款动作.
+     *
+     * @param amt      the amt
+     * @param modelMap the model map
+     * @return the string
+     */
     @RequestMapping("/pay/mobile")
-    public String payInMobileSite(@RequestParam(name = "amt")String amt,
+    public String payInMobileSite(@RequestParam(name = "amt",defaultValue = "0.01")String amt,
                                   ModelMap modelMap){
 
         try {
@@ -84,44 +107,42 @@ public class AlipayController {
             request.setNotifyUrl("http://你的异步处理地址/alipay/notify_mobile");
             request.setReturnUrl("http://你的同步处理地址/alipay/alipay_sync_mobile");
             AlipayMobileSiteRequest.BizContent bizContent = new AlipayMobileSiteRequest.BizContent();
-            bizContent.setTotalAmount("0.01");
+            bizContent.setTotalAmount(amt);
             bizContent.setSubject("测试H5(手机网页)支付");
             //bizContent.setSeller_id(partner_id);
             bizContent.setProductCode("p0001");
             bizContent.setOutTradeNo(System.currentTimeMillis()+"");
             request.setBizContent(bizContent);
             String html = AlipayClient.payInMobileSite(request,alipayCore);
-
             modelMap.addAttribute("form", html);
-
-
-
         } catch (Exception e) {
-            log.error(e);
+            log.error("手机网站支付失败",e);
         }
-
         return "alipay/alipay_mobile";
     }
 
+    /**
+     * 电脑网站支付.FIXME 暂不可用
+     *
+     * @param amt      the amt
+     * @param modelMap the model map
+     * @return the string
+     */
     @RequestMapping("/pay/web")
-    public String payInWebSite(@RequestParam(name = "amt")String amt,
+    public String payInWebSite(@RequestParam(name = "amt",defaultValue = "0.01")String amt,
                                ModelMap modelMap){
-
-
         try {
-
             AlipayCore.ClientBuilder clientBuilder = new AlipayCore.ClientBuilder();
             AlipayCore alipayCore = clientBuilder.setAlipayPublicKey(alipayConfig.getAlipay_public_key())
                     .setAppId(alipayConfig.getAppid())
                     .setPrivateKey(alipayConfig.getPrivate_key())
                     .setSignType(AlipayConstants.SIGN_TYPE_RSA2).build();
 
-
             AlipayWebSiteRequest request = new AlipayWebSiteRequest();
             request.setNotifyUrl("http://你的异步处理地址/alipay/notify_mobile");
             request.setReturnUrl("http://你的同步处理地址/alipay/alipay_sync_web");
             AlipayWebSiteRequest.BizContent bizContent = new AlipayWebSiteRequest.BizContent();
-            bizContent.setTotalAmount("0.01");
+            bizContent.setTotalAmount(amt);
             bizContent.setSubject("测试电脑网站支付");
             bizContent.setBody("测试");
             bizContent.setProductCode("p0001");
@@ -130,20 +151,44 @@ public class AlipayController {
             modelMap.addAttribute("form", html);
 
         } catch (Exception e) {
-            log.error(e);
+            log.error("电脑网站支付失败",e);
         }
-
 
         return "alipay/alipay_web";
     }
 
-    @RequestMapping("/refund")
+    /**
+     * 查询,适用所有付款方式.
+     *
+     * @param outTradeNo the out trade no
+     * @return the object
+     */
+    @RequestMapping(value = "/query",produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     @ResponseBody
-    public AlipayRefundResponse refund(@RequestParam(name = "amt")String amt,
+    public Object query(@RequestParam(name = "out_trade_no")String outTradeNo){
+
+        try {
+            AlipayCore.ClientBuilder clientBuilder = new AlipayCore.ClientBuilder();
+            AlipayCore alipayCore = clientBuilder.setAlipayPublicKey(alipayConfig.getAlipay_public_key())
+                    .setAppId(alipayConfig.getAppid())
+                    .setPrivateKey(alipayConfig.getPrivate_key())
+                    .setSignType(AlipayConstants.SIGN_TYPE_RSA2).build();
+
+            AlipayQueryResponse response = AlipayClient.query(outTradeNo,alipayCore);
+            return response;
+        } catch (Exception e) {
+            log.error("支付宝查询失败.",e);
+            return e;
+        }
+    }
+
+
+    @RequestMapping(value = "/refund",produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @ResponseBody
+    public Object refund(@RequestParam(name = "amt")String amt,
                          @RequestParam(name = "out_trade_no")String outTradeNo){
 
         try {
-
             AlipayCore.ClientBuilder clientBuilder = new AlipayCore.ClientBuilder();
             AlipayCore alipayCore = clientBuilder.setAlipayPublicKey(alipayConfig.getAlipay_public_key())
                     .setAppId(alipayConfig.getAppid())
@@ -156,15 +201,19 @@ public class AlipayController {
             request.setRefundReason("测试退款");
             AlipayRefundResponse response = AlipayClient.refund(request,alipayCore);
             return response;
-
         } catch (Exception e) {
-            log.error(e);
+            log.error("支付宝退款失败.",e);
+            return e;
         }
-
-        return null;
     }
 
 
+    /**
+     * 手机网站支付同步跳转页面
+     *
+     * @param request the request
+     * @return the string
+     */
     @RequestMapping("/alipay_sync_mobile")
     public String sync_mobile(HttpServletRequest request){
 
@@ -179,9 +228,11 @@ public class AlipayController {
     }
 
 
+    /**
+     * 电脑网站支付同步跳转页面.
+     */
     @RequestMapping("/alipay_sync_web")
     public String sync_web(HttpServletRequest request){
-
         System.out.println("==============sync_web=====================");
         Enumeration<String> its = request.getParameterNames();
         while (its.hasMoreElements()){
